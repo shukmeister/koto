d1b3bb04ba93738404354365a428c007b5860eff
Ben Shukman <shukipost@gmail.com>
2015-12-04 23:30:20 -0800

    Gmail API credentials fetch

--- quickstart.py
+++ quickstart.py
@@ -1,76 +1,3 @@
-#koto - communication tracking utiliity
-#created by ben shukman
-
-#each name has attributes: last date contacted, each contact is a commit (so its a unique data struct)
-
-#store JSON / class of data
-
-#import list of names
-#if recent email, display in high priority
-#if name is under X days old, needs love
-
-'''
-
-template = ['jon', 'murloc', 'morty']
-personList = []
-count = 0
-
-class person:
-	def __init__(self):
-		self.firstName = None
-		self.lastName = None
-		self.numOfCommits = 0
-		self.lastContact = None
-	#personType = friend
-	#make friend into a class?
-	#firstName
-	#lastName
-	#numOfCommits
-	#lastContact
-	#type - friend, associate, investor, coworker (make these into classes?)
-
-godzilla = person()
-woz = person()
-belka = person()
-
-testList = [godzilla, woz, belka]
-# testList.append(godzilla)
-
-godzilla.firstName = "godzilla"
-godzilla.lastContact = 4
-
-# initialization:
-for x in template:
-	# print x
-	x = person()
-	x.firstName = str(x)
-	# print x.firstName
-	x.lastContact = count
-	count += 1
-	personList.append(x)
-
-# print personList[0]
-
-# if 'koto status':
-for z in testList:
-	#make each unit in the list a person class
-	print str(z.firstName) + '\n' + '- days since last communication: ' + str(z.lastContact) + '\n'
-
-for z in personList:
-	print str(z.firstName) + '\n' + '- days since last communication: ' + str(z.lastContact) + '\n'
-
-class commit:
-	pass
-	#id = idGen(name, date)
-	#date
-	#type - email, text, phone, fb, meeting
-	#subject
-	#body
-
-'''
-
-#make preferences tab
-
 from __future__ import print_function
 import httplib2
 import os
@@ -119,16 +46,25 @@ def get_credentials():
     return credentials
 
 def main():
-	#fetch google account credentials
+    """Shows basic usage of the Gmail API.
+
+    Creates a Gmail API service object and outputs a list of label names
+    of the user's Gmail account.
+    """
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('gmail', 'v1', http=http)
 
+    # results = service.users().labels().list(userId='me').execute()
+    # labels = results.get('labels', [])
 
+    # if not labels:
+    #     print('No labels found.')
+    # else:
+    #   print('Labels:')
+    #   for label in labels:
+    #     print(label['name'])
 
-def idGen(name, date):
-	#generate id name such as 8839GODZILLA040494
-	pass
 
 if __name__ == '__main__':
     main()
